4e004f878e3843ec7a7f5de6184ba19a3e2d2bd7
Martin 'kokesak' Litwora <mlitwora@redhat.com>
2024-04-17 13:01:01 +0200

    Add mocking kmod inhibitor and infinit sleep

--- convert2rhel.py
+++ convert2rhel.py
@@ -2,14 +2,25 @@
 import os
 import subprocess
 import sys
+import time
 
-DUMP_ENV_VARS = "/tmp/c2r_mock_dump_env_vars"
+# Based on the existence of the following files do a certain thing
+MOCK_DUMP_ENV_VARS = "/tmp/c2r_mock_dump_env_vars"
+MOCK_INFINITE_LOOP = "/tmp/c2r_mock_infinite_loop"
+MOCK_KMOD_INHIBITOR = "/tmp/c2r_mock_kmod_inhibitor"
+
+# The output of a mock to check the test result
 MOCK_OUTPUT_FILE = "/tmp/c2r_mock_test.out"
+
+# Script mode as defined in the script
+# (https://github.com/oamg/convert2rhel-insights-tasks/blob/main/scripts/c2r_script.py#L15)
 SCRIPT_MODE = os.environ.get("SCRIPT_MODE", None)
 
-ANALYZE_NO_ISSUE_FILE_URL = "/usr/share/convert2rhel/data/analyze/convert2rhel-pre-conversion.json"
+# Report json files as expected by script
+ANALYZE_NO_ISSUE_FILE = "/usr/share/convert2rhel/data/analyze/convert2rhel-pre-conversion.json"
+ANALYZE_KMOD_INHIBITOR_FILE = "/usr/share/convert2rhel/data/kmod/convert2rhel-pre-conversion.json"
 C2R_LOG_FOLDER = "/var/log/convert2rhel"
-C2R_PRE_CONVERSION_JSON_LOG_LOCATION = C2R_LOG_FOLDER + "/convert2rhel-pre-conversion.json"
+C2R_ANALYZE_JSON_LOG_LOCATION = C2R_LOG_FOLDER + "/convert2rhel-pre-conversion.json"
 
 
 def create_log_folder():
@@ -18,28 +29,29 @@ def create_log_folder():
         os.makedirs(C2R_LOG_FOLDER)
 
 
-def create_successful_report():
+def create_report(reportfile):
     """
     Download and put the c2r report file in the log directory
     where the rhc-worker-script parse the result of the c2r run
     """
     create_log_folder()
-    if SCRIPT_MODE == "ANALYSIS":
-        cmd = ["cp", ANALYZE_NO_ISSUE_FILE_URL, C2R_PRE_CONVERSION_JSON_LOG_LOCATION]
-        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1)
-        output = ""
-        for line in iter(process.stdout.readline, b""):
-            line = line.decode("utf8")
-            output += line
+    cmd = ["cp", reportfile, C2R_ANALYZE_JSON_LOG_LOCATION]
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1)
+    output = ""
+    for line in iter(process.stdout.readline, b""):
+        line = line.decode("utf8")
+        output += line
 
-        # Wait for the process to end
-        process.wait()
+    # Wait for the process to end
+    process.wait()
 
-        return output, process.returncode
+    return output, process.returncode
 
 
 def main():
     """Main script logic"""
+
+    script_es = 0
 
     if SCRIPT_MODE not in ("ANALYSIS", "CONVERSION"):
         print("SCRIPT_MODE envar is not one of the expected, got: {}".format(SCRIPT_MODE))
@@ -47,19 +59,36 @@ def main():
 
     # Decide what to do based on existence of a specific file.
     # This serves as a communication with a running test.
-    if os.path.isfile(DUMP_ENV_VARS):
+    if os.path.isfile(MOCK_DUMP_ENV_VARS):
         with open(MOCK_OUTPUT_FILE, mode="w") as f:
             f.write(str(os.environ) + "\n")
 
-        output, return_code = create_successful_report()
-        if return_code != 0:
+        output, rc = create_report(ANALYZE_NO_ISSUE_FILE)
+        if rc != 0:
             print(output)
+
+    elif os.path.isfile(MOCK_INFINITE_LOOP):
+        while True:
+            # Infinitely sleep and write every 5 seconds actual time.
+            time.sleep(5)
+            with open(C2R_LOG_FOLDER + "/convert2rhel.log", mode="a") as f:
+                f.write(time.ctime() + "\n")
+
+    elif os.path.isfile(MOCK_KMOD_INHIBITOR):
+        output, rc = create_report(ANALYZE_KMOD_INHIBITOR_FILE)
+        if rc != 0:
+            print(output)
+        if SCRIPT_MODE == "CONVERSION":
+            script_es = 1
+
     else:
         print(
-            "Script do now know what to do.. :( \
+            "Script do now know what to do.. :(\n \
              create one of the specific files to tell it what to do."
         )
         sys.exit(10)
+
+    sys.exit(script_es)
 
 
 if __name__ == "__main__":
