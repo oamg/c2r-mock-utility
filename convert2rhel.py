cee75500d64f44f3ac62386d06a572a83d7fc079
Sergio Santamaria <13274437+SerCantus@users.noreply.github.com>
2024-09-16 11:04:28 +0200

    Add parsed arguments to envvars (#11)
    * Add parsed arguments to envvars
    
    * Cast Namespace to dict
    
    * Make envvars saved strings
    
    * Move parsed envvars dump outside parsing function

--- convert2rhel.py
+++ convert2rhel.py
@@ -141,6 +141,10 @@ def main():
     # Decide what to do based on existence of a specific file.
     # This serves as a communication with a running test.
     if os.path.isfile(MOCK_DUMP_ENV_VARS):
+        parsed_arguments_dict = vars(parsed_opts)
+        for key, value in parsed_arguments_dict.items():
+            os.environ[key] = str(value)
+
         with open(MOCK_OUTPUT_FILE, mode="w") as f:
             json.dump(dict(os.environ), f)
 
